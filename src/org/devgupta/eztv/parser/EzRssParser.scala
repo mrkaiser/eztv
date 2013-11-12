5af22df4dce9dda812429a7a7255a4bc2eb63c96
Dev Gupta <kaiserollofdoom@gmail.com>
2013-11-12 15:14:33 -0600

    Added a super sweet function that gives me the top video given a map.

--- src/org/devgupta/eztv/parser/EzRssParser.scala
+++ src/org/devgupta/eztv/parser/EzRssParser.scala
@@ -15,13 +15,18 @@ object EzRssParser {
   def rssToList(url: String): List[Video] = {
     //get the rss
     val rss = XML.load(new URL(url))
-    (rss \\ "item").map(EztvParser.nodeToVideo(_)).toList
+     rss \\ "item" map(EztvParser.nodeToVideo(_)) toList
 
   }
 
   def uniqueList(videoList:List[Video]):Map[String, Video] = {
-    //groupby name+season+epsideo (so videos with different qualities get grouped together). Then get only the first video.
+    //groupby name+season+episode (so videos with different qualities get grouped together).
+    // Then get only the first video. (1080 will come before 720 which will come before HDTV)
     videoList.groupBy(x => x.series+x.season+x.episode).mapValues(_(0))
+  }
+
+  def newestVideo(videoList:Map[String,Video]):Video = {
+    videoList.toList.sortBy(x => x._2.season*100 + x._2.episode).head._2
   }
 
 
